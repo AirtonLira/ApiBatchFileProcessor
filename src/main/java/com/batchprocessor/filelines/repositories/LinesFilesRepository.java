7623c1c559491028b579dd397a9f172fe82dcb6e
Airton Lira jr <airton.lirajunior@bemobi.com>
2023-09-23 13:45:25 -0300

    change entitties

--- src/main/java/com/batchprocessor/filelines/repositories/LinesFilesRepository.java
+++ src/main/java/com/batchprocessor/filelines/repositories/LinesFilesRepository.java
@@ -2,16 +2,10 @@ package com.batchprocessor.filelines.repositories;
 
 import com.batchprocessor.filelines.entities.LinesFile;
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-=======
-
-
-
->>>>>>> origin/main
 public interface LinesFilesRepository extends JpaRepository<LinesFile, Long> {
 
 }
